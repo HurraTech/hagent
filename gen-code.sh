290806b52da40cd5eb84c130291ecda5ccc68291
aimannajjar <aiman.najjar@hurranet.com>
2020-09-30 00:19:13 -0400

    clenaups

--- gen-code.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-protoc -I . hurra_agent.proto --go_out=plugins=grpc:./proto 
-protoc -I . hurra_agent.proto --go_out=plugins=grpc:../../jawhar/internal/agent/proto
-grpc_tools_ruby_protoc -I . --ruby_out=../jawhar/lib/hurracloud-agent --grpc_out=../jawhar/lib/hurracloud-agent hurra_agent.proto
-# cp hurra_agent.proto ../jawhar/
