7c04218fc16af39cbd1ea8d892bff5f1440fe8b5
Aiman Najjar <aiman.najjar@hurranet.com>
2021-05-13 15:54:21 +0300

    stats per second

--- disk/disk_darwin.go
+++ disk/disk_darwin.go
@@ -1,7 +1,9 @@
 // +build darwin
+
 package disk
 
 import (
+	"fmt"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -16,44 +18,42 @@ type DiskStats struct {
 }
 
 func Get() (*DiskStats, error) {
-	output, err := exec.Command("iostat", "-d").Output()
+	output, err := exec.Command("iostat", "-d", "-c", "2", "-w", "1").Output()
 	if err != nil {
 		return nil, err
 	}
 	log.Debugf("iostat command output: %s", output)
 
 	lines := strings.Split(string(output), "\n")
+	line := strings.TrimSpace(lines[3])
+	log.Debugf("Parsing iostat output line %s", line)
 	var stats DiskStats
-	for _, line := range lines[2:] {
-		line = strings.TrimSpace(line)
-		log.Debugf("Parsing iostat output line %s", line)
-		stat := strings.Fields(line)
-		if len(stat) < 3 {
-			log.Debugf("iostat output line '%s' looks invalid (returned # of fields: %d), skipping it. ", line, len(stat))
-			continue
-		}
-		kps, err := strconv.ParseFloat(stat[0], 10)
-		if err != nil {
-			log.Errorf("Could not parse iostat kps command output line: %s: %s", line, err)
-			continue
-		}
-
-		tps, err := strconv.ParseFloat(stat[1], 10)
-		if err != nil {
-			log.Errorf("Could not parse iostat tps command output line: %s: %s", line, err)
-			continue
-		}
-
-		mps, err := strconv.ParseFloat(stat[2], 10)
-		if err != nil {
-			log.Errorf("Could not parse iostat mbps command output line: %s: %s", line, err)
-			continue
-		}
-
-		log.Infof("iostats: %d, %d, %d", kps, tps, mps)
-
-		stats.ReadsPerSecond += uint64(kps * tps * 1024)
-		stats.WritesPerSecond += uint64(mps * 1024 * 1024)
+	stat := strings.Fields(line)
+	if len(stat) < 3 {
+		log.Debugf("iostat output line '%s' looks invalid (returned # of fields: %d), skipping it. ", line, len(stat))
+		return nil, fmt.Errorf("iostat output line '%s' looks invalid (returned # of fields: %d), skipping it. ", line, len(stat))
 	}
+	kps, err := strconv.ParseFloat(stat[0], 10)
+	if err != nil {
+		log.Errorf("Could not parse iostat kps command output line: %s: %s", line, err)
+		return nil, fmt.Errorf("Could not parse iostat kps command output line: %s: %s", line, err)
+	}
+
+	tps, err := strconv.ParseFloat(stat[1], 10)
+	if err != nil {
+		log.Errorf("Could not parse iostat tps command output line: %s: %s", line, err)
+		return nil, fmt.Errorf("Could not parse iostat tps command output line: %s: %s", line, err)
+	}
+
+	mps, err := strconv.ParseFloat(stat[2], 10)
+	if err != nil {
+		log.Errorf("Could not parse iostat mbps command output line: %s: %s", line, err)
+		return nil, fmt.Errorf("Could not parse iostat mbps command output line: %s: %s", line, err)
+	}
+
+	log.Debugf("iostats: %d, %d, %d", kps, tps, mps)
+
+	stats.ReadsPerSecond += uint64(kps * tps * 1024)
+	stats.WritesPerSecond += uint64(mps * 1024 * 1024)
 	return &stats, nil
 }
